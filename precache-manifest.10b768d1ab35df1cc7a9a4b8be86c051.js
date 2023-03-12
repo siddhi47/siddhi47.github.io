8dcdd21e789569d7fa821cfa375fd2afe310aa2e
siddhi <siddhi@sid.localdomain>
2023-03-11 18:45:46 -0600

    Updates

---
+++ precache-manifest.10b768d1ab35df1cc7a9a4b8be86c051.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36ebf9b6ecb78c9e6722696bf38cc19f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8a849567b6694781f7e",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "ecfc0c7f066abb454ca4",
+    "url": "/static/css/main.db6f0745.chunk.css"
+  },
+  {
+    "revision": "e8a849567b6694781f7e",
+    "url": "/static/js/2.02a57c3c.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.02a57c3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecfc0c7f066abb454ca4",
+    "url": "/static/js/main.4240d01d.chunk.js"
+  },
+  {
+    "revision": "4d0591cb4c4c8ff71e5f",
+    "url": "/static/js/runtime-main.9f0dda3d.js"
+  }
+]);
